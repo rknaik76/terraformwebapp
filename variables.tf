62c86b599acfd3574fcc4bed19b5854622567186
Ranjit Naik <rknaik76@gmail.com>
2021-07-18 09:47:50 +0100

    pipeline yaml change

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "subscription_id" {
-  
+  default = $(TF_VAR_subscription_id)
 }
 
 variable "client_id" {
-  
+  default = $(TF_VAR_client_id)
 }
 
 variable "client_secret" {
-  
+  default = $(TF_VAR_client_secret)
 }
 
 variable "tenant_id" {
-  
+  default = $(TF_VAR_tenant_id)
 }
 
 variable "environment" {
