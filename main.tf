52f6ac5dce74d85751896dc535f064cf8ccff2da
rknaik76 <rknaik76@gmail.com>
2021-08-04 11:16:00 +0100

    t

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
   # version = ">2.4.0"
-  subscription_id = var.subscription_id
-  client_id       = var.client_id 
-  client_secret   = var.client_secret  
-  tenant_id       = var.tenant_id
+  #subscription_id = var.subscription_id
+  #client_id       = var.client_id 
+  #client_secret   = var.client_secret  
+  #tenant_id       = var.tenant_id
   features {}
 }
 
